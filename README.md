fb1a6f83844316534453e9574d2bca3d924819ce
DuyNamUET <duynamk63uet@gmail.com>
2020-03-30 22:36:20 +0700

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+## Movement
+Type of motion: Two wheeled differential drive
+Motor type: Stepper motor
--- src/convertVel2SpeedMotor.cpp
+++ src/convertVel2SpeedMotor.cpp
@@ -1,15 +1,13 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include "omni_wheel_ros/SpeedMotor.h"
+#include <std_msgs/Int8>
 
 // define some specification of robot
-#define VELMAX 12   // speed max of motor
-#define L 10        // distance from center robot to center wheel
-#define R 4.8/2     // radius of omni wheel
+
 
 geometry_msgs::Twist vel;
 void robotSetVelSub(const geometry_msgs::Twist::ConstPtr& msg);
-omni_wheel_ros::SpeedMotor convertVel2Speed(const geometry_msgs::Twist& robot_vel);
+void convertVel2Speed(const geometry_msgs::Twist& robot_vel, int& spl, int& spr);
 
 int main(int argc, char**argv)
 {
@@ -21,14 +19,17 @@ int main(int argc, char**argv)
     ros::Subscriber sub_set_vel = nh.subscribe("/robot/cmd_vel", 1000, &robotSetVelSub);
 
     // publish speed of each motor
-    ros::Publisher pub_speed = nh.advertise<omni_wheel_ros::SpeedMotor>("/speed_motor", 1000);  
+    ros::Publisher pub_speed_left = nh.advertise<std_msgs::Int8>("/speed_left", 100);
+    ros::Publisher pub_speed_right = nh.advertise<std_msgs::Int8>("/spped_right", 100);
     
     ros::Rate r(1000);
     while (nh.ok())
     {
+        int spl, spr;
         // convert robot's vel to speed of each motor
-        omni_wheel_ros::SpeedMotor sp = convertVel2Speed(vel);
-        pub_speed.publish(sp);
+        convertVel2Speed(vel, spl, spr);
+        pub_speed_left.publish(spl);
+        pub_speed_right.publish(spr);
         r.sleep();
         ros::spinOnce();
     }
@@ -39,24 +40,10 @@ int main(int argc, char**argv)
 void robotSetVelSub(const geometry_msgs::Twist::ConstPtr& msg)
 {
     vel = *msg;
+    return;
 }
 
-omni_wheel_ros::SpeedMotor convertVel2Speed(const geometry_msgs::Twist& robot_vel)
+void convertVel2Speed(const geometry_msgs::Twist& robot_vel, int& spl, int& spr)
 {
-    // get value from vel
-    float l_x = robot_vel.linear.x;
-    float l_y = robot_vel.linear.y;
-    float a_z = robot_vel.angular.z;
-
-    // convert 2 velocity of each motor
-    float v1 = 2*l_y/3 + L*a_z/3;
-    float v2 = -l_x/sqrt(3) - l_y/3 + L*a_z/3;
-    float v3 = l_x/sqrt(3) - l_y/3 + L*a_z/3;
-
-    // convert 2 speed of each motor (0-255)
-    omni_wheel_ros::SpeedMotor sp;
-    sp.speed1 = 255*v1/VELMAX/R;
-    sp.speed2 = 255*v2/VELMAX/R;
-    sp.speed3 = 255*v3/VELMAX/R;
-    return sp;
+    return;
 }
