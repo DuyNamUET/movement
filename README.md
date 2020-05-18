3579112ab6cc57c4b3cd0c2cf061aba273b1c443
DuyNamUET <duynamk63uet@gmail.com>
2020-05-18 08:24:10 +0700

    Update .ino file and README

--- README.md
+++ README.md
@@ -4,25 +4,33 @@ Motor type: Stepper motor <br>
 Stepper motor name: 28BYJ-48 <br>
 Driver: ULN2003 <br>
 Power input: 5V <br>
-Max speed of stepper in this input about 20-25rpm (need to check) <br>
+Max speed of stepper: 19rpm (check again with your input) <br>
 
 ## Step to step
 #### Control Stepper Motor with Arduino <br> 
-[28BYJ-48 Stepper Motor with ULN2003 Driver and Arduino Tutorial](https://www.makerguides.com/28byj-48-stepper-motor-arduino-tutorial/)
+* [28BYJ-48 Stepper Motor with ULN2003 Driver and Arduino Tutorial](https://www.makerguides.com/28byj-48-stepper-motor-arduino-tutorial/)
 #### ROS Arduino <br>
-[Installation](http://wiki.ros.org/rosserial_arduino/Tutorials/Arduino%20IDE%20Setup) <br>
-[Tutorials](http://wiki.ros.org/rosserial_arduino/Tutorials) <br>
+* [Installation](http://wiki.ros.org/rosserial_arduino/Tutorials/Arduino%20IDE%20Setup) <br>
+* [Tutorials](http://wiki.ros.org/rosserial_arduino/Tutorials) <br>
 #### Control Stepper Motor with ROS Arduino <br>
-Download and install in your workspace<br>
+1. Download and install in your workspace<br>
 ```
 $ cd [your_ws]/src
 $ git clone https://github.com/DuyNamUET/movement
 $ cd ..
 $ catkin_make
 ```
-Run
-Firstly, check your port Arduino and change parameter on launch file <br>
-Then
+2. Run <br>
+* Firstly, check your port Arduino and change parameter on launch file. Here, it is ```/dev/ttyACM0``` <br>
+```
+<launch>
+    <node pkg="rosserial_python" type="serial_node.py" name="serial_node"
+            args="/dev/ttyACM0" respawn="true" />
+    <node pkg="movement" type="control_directly" name="control_directly" args=""
+            output="screen" />
+</launch>
+```
+* Then:
 ```
 $ roslaunch movement run_motor.launch
 ```
--- control_motor/control_motor.ino
+++ control_motor/control_motor.ino
@@ -10,10 +10,10 @@
 #define stepLPin3 10    //IN3
 #define stepLPin4 11    //IN4
 // right stepper pin definitions
-//#define stepRPin1 8     //IN1
-//#define stepRPin2 9     //IN2
-//#define stepRPin3 10    //IN3
-//#define stepRPin4 11    //IN4
+#define stepRPin1 4     //IN1
+#define stepRPin2 5     //IN2
+#define stepRPin3 6     //IN3
+#define stepRPin4 7     //IN4
 
 // define max step per second of stepper motor
 #define MAX_SPEED 1216
@@ -24,8 +24,8 @@
 // initialize 
 AccelStepper stepL = AccelStepper(MotorInterfaceType, stepLPin1, 
                                     stepLPin3, stepLPin2, stepLPin4);
-//AccelStepper stepR = AccelStepper(MotorInterfaceType, stepRPin1, 
-//                                    stepRPin2, stepRPin3, stepRPin4);
+AccelStepper stepR = AccelStepper(MotorInterfaceType, stepRPin1, 
+                                   stepRPin3, stepRPin2, stepRPin4);
 
 // initialize ros
 ros::NodeHandle nh;
@@ -35,33 +35,33 @@ void speedLeftSub(const std_msgs::Int16& msg)
     spl = msg.data;
 }
 
-//void speedRightSub(const std_msgs::Int16& msg)
-//{
-//    spr = msg.data;
-//}
+void speedRightSub(const std_msgs::Int16& msg)
+{
+    spr = msg.data;
+}
 
 ros::Subscriber<std_msgs::Int16> spl_sub("/speed_left",&speedLeftSub);
-//ros::Subscriber<std_msgs::Int16> spr_sub("/speed_right",&speedRightSub);
+ros::Subscriber<std_msgs::Int16> spr_sub("/speed_right",&speedRightSub);
 
 void setup()
 {
     // init node and subscribe topic
     nh.initNode();
     nh.subscribe(spl_sub);
-//    nh.subscribe(spr_sub);
+    nh.subscribe(spr_sub);
 
     // set max steps per second
     stepL.setMaxSpeed(MAX_SPEED);
-//    stepR.setMaxSpeed(MAX_SPEED);
+    stepR.setMaxSpeed(MAX_SPEED);
 }
 
 void loop()
 {
     // set speed and run
     stepL.setSpeed(spl);
-//    stepR.setSpeed(spr);
+    stepR.setSpeed(spr);
     stepL.runSpeed();
-//    stepR.runSpeed();
+    stepR.runSpeed();
     
     nh.spinOnce();
 }
